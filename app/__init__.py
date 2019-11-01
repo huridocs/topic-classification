e099507096f6061dc987429c4f42225d19586588
Samantha Schaevitz <sam@huridocs.org>
2019-11-01 17:52:21 +0100

    Set-up a flask configuration file and wiring it up to the server run action.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,32 @@
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
+from app import config
+
+
+CONFIG = {
+    "development": "app.config.DevelopmentConfig",
+    "test": "app.config.TestConfig",
+    "production": "app.config.ProductionConfig",
+    "default": "app.config.DevelopmentConfig",
+}
+
+app = Flask(__name__,)
+
+# initialize configuration values
+app.logger.debug("Reading configuration...")
+config_name = os.getenv("FLASK_ENV", 'default')
+app.logger.debug("FLASK_ENV is " + config_name)
+
+config_obj = CONFIG[config_name]
+app.logger.debug("config obj: " + config_obj)
+app.config.from_object(config_obj)
+
+if app.config.from_pyfile('config.py'):
+    app.logger.info("Successfully read instance configuration file.")
+app.logger.info("Flask config variables:")
+app.logger.info(app.config.items())
 
 from app import routes  # nopep8
 from app import task_routes  # nopep8
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from app import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, threaded=True)
