a812934a34b101aa0c8336ad2250acf468158487
Benjamin Dittes <bdittes@google.com>
2019-11-15 13:34:37 +0100

    tiny fixes

--- app/flask_config.py
+++ app/flask_config.py
@@ -8,7 +8,7 @@ class Config(object):
     BERT = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
     MODEL = "UPR_2percent_ps0"
     INSTANCE = "1573031002"
-    PATH_TO_GCS_CREDS = "./.credz/BERT Classification-9a8b5ef88627.json"
+    PATH_TO_GCS_CREDS = "./.credz/bert-classification-key.json"
     MODEL_CONFIG_PATH = "./static/model_config.json"
 
 
